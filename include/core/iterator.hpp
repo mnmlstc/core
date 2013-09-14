e3f10065f357fa826bc8ad79925cccec24ffd862
Tres Walsh <tres.walsh@mnmlstc.com>
2013-09-13 20:45:55 -0700

    Add crend, crbegin, rbegin, and rend to core/iterator.hpp

--- include/core/iterator.hpp
+++ include/core/iterator.hpp
@@ -16,6 +16,36 @@ auto cend (Container const& container) -> decltype(std::end(container)) {
   return std::end(container);
 }
 
+template <class Container>
+auto rbegin (Container const& container) -> decltype(container.rbegin()) {
+  return container.rbegin();
+}
+
+template <class Container>
+auto rbegin (Container& container) -> decltype(container.rbegin()) {
+  return container.rbegin();
+}
+
+template <class Container>
+auto crbegin (Container const& container) -> decltype(rbegin(container)) {
+  return rbegin(container);
+}
+
+template <class Container>
+auto rend (Container const& container) -> decltype(container.rend()) {
+  return container.rend();
+}
+
+template <class Container>
+auto rend (Container& container) -> decltype(container.rend()) {
+  return container.rend();
+}
+
+template <class Container>
+auto crend (Container const& container) -> decltype(rend(container)) {
+  return rend(container);
+}
+
 }} /* namespace core::v1 */
 
 #endif /* CORE_ITERATOR_HPP */
