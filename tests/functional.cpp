e641172768adf66e77c73f100068a53eb1096728
Tres Walsh <tres.walsh@mnmlstc.com>
2013-09-22 17:21:01 -0700

    Add test for runtime unpacking an unordered map

--- tests/functional.cpp
+++ tests/functional.cpp
@@ -13,7 +13,23 @@ int main () {
   using namespace unittest;
 
   test("functional") = {
-    task("invoke-runtime-unpack-unordered-map") = [] { assert::fail(); },
+    task("invoke-runtime-unpack-unordered-map") = [] {
+      std::unordered_map<int, std::string> values = {
+        { 0, "first" },
+        { 1, "second" },
+        { 2, "third" }
+      };
+
+      auto result = core::invoke(
+        core::runpack<3> { },
+        [](std::string& a, std::string& b, std::string& c) -> std::string {
+          return a + b + c;
+        },
+        values
+      );
+
+      assert::equal(result, std::string { "firstsecondthird" });
+    },
     task("invoke-runtime-unpack-basic-string") = [] { assert::fail(); },
     task("invoke-runtime-unpack-vector") = [] { assert::fail(); },
     task("invoke-runtime-unpack-deque") = [] { assert::fail(); },
