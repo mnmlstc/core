b8a162f42e89f504637510756adad6ebc3a39b91
Tres Walsh <tres.walsh@mnmlstc.com>
2013-09-24 14:07:29 -0700

    Change semantics for runtime unpacking.
    Calling core::invoke with the 'runpack' sentinel will use
    function_traits to figure out the arity of the functor and then generate
    the necessary index_sequence based off of this information. This has
    resulted in the removal of runtime unpacking an entire container that
    would take the first element in the container, then unpack the rest of the
    container into the first element. It is considered ok for this obscure
    use to be lost

--- include/core/functional.hpp
+++ include/core/functional.hpp
@@ -22,13 +22,18 @@ template <class C, class R, class... Args>
 struct function_traits<R(C::*)(Args...)> : function_traits<R(C&, Args...)> { };
 
 template <class C, class R, class... Args>
-struct function_traits<R(C::*)(Args...) const> :
-  function_traits<R(C const&, Args...)>
+struct function_traits<R(C::*)(Args...) const volatile> :
+  function_traits<R(C volatile const&, Args...)>
 { };
 
-template <class R, class... Args>
-struct function_traits<std::function<R(Args...)>> :
-  function_traits<R(Args...)>
+template <class C, class R, class... Args>
+struct function_traits<R(C::*)(Args...) volatile> :
+  function_traits<R(C volatile&, Args...)>
+{ };
+
+template <class C, class R, class... Args>
+struct function_traits<R(C::*)(Args...) const> :
+  function_traits<R(C const&, Args...)>
 { };
 
 template <class R, class... Args>
@@ -39,6 +44,14 @@ struct function_traits<R(Args...)> {
 
   template <std::size_t N>
   using argument = typename std::tuple_element<N, std::tuple<Args...>>::type;
+};
+
+template <class F> struct function_traits {
+  using functor_type = function_traits<decltype(&decay_t<F>::operator())>;
+  using return_type = typename functor_type::return_type;
+  static constexpr std::size_t arity = functor_type::arity - 1;
+  template <std::size_t N>
+  using argument = typename functor_type::template argument<N>;
 };
 
 template <class Functor, class Object, class... Args>
@@ -74,8 +87,9 @@ constexpr auto invoke (Functor&& functor, Object&& object) -> enable_if_t<
 > { return (*std::forward<Object>(object)).*functor; }
 
 template <class Functor, class... Args>
-constexpr auto invoke (Functor&& functor, Args&&... args) -> invoke_of_t<
-  Functor, Args...
+constexpr auto invoke (Functor&& functor, Args&&... args) -> enable_if_t<
+  invokable<Functor, Args...>::value,
+  decltype(std::forward<Functor>(functor)(std::forward<Args>(args)...))
 > { return std::forward<Functor>(functor)(std::forward<Args>(args)...); }
 
 namespace impl {
@@ -102,18 +116,13 @@ invoke_of_t<Functor, decltype(std::forward<U>(runpackable).at(I))...>
     std::forward<U>(runpackable).at(I)...);
 }
 
-template <class U, std::size_t... I>
-auto runpack (U&& runpackable, index_sequence<I...>&&) -> invoke_of_t<
-  decltype(std::forward<U>(runpackable).at(I))...
-> { return ::core::v1::invoke(std::forward<U>(runpackable).at(I)...); }
-
 } /* namespace impl */
 
 struct unpack_t final { };
 constexpr unpack_t unpack { };
 
-template <std::size_t N>
-using runpack = std::integral_constant<std::size_t, N>;
+struct runpack_t final { };
+constexpr runpack_t runpack { };
 
 template <class Functor, class Unpackable>
 constexpr auto invoke (unpack_t, Functor&& functor, Unpackable&& unpackable) ->
@@ -151,9 +160,9 @@ enable_if_t<
   );
 }
 
-template <class Functor, class Runpackable, std::size_t N>
+template <class Functor, class Runpackable>
 constexpr auto invoke (
-  runpack<N>,
+  runpack_t,
   Functor&& functor,
   Runpackable&& unpackable
 ) -> enable_if_t<
@@ -162,30 +171,14 @@ constexpr auto invoke (
     impl::runpack(
       std::forward<Functor>(functor),
       std::forward<Runpackable>(unpackable),
-      make_index_sequence<N> { }
+      make_index_sequence<function_traits<Functor>::arity> { }
     )
   )
 > {
   return impl::runpack(
     std::forward<Functor>(functor),
     std::forward<Runpackable>(unpackable),
-    make_index_sequence<N> { }
-  );
-}
-
-template <class Runpackable, std::size_t N>
-constexpr auto invoke (runpack<N>, Runpackable&& unpackable) -> enable_if_t<
-  is_runpackable<decay_t<Runpackable>>::value,
-  decltype(
-    impl::runpack(
-      std::forward<Runpackable>(unpackable),
-      make_index_sequence<N> { }
-    )
-  )
-> {
-  return impl::runpack(
-    std::forward<Runpackable>(unpackable),
-    make_index_sequence<N> { }
+    make_index_sequence<function_traits<Functor>::arity> { }
   );
 }
 
