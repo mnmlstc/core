92de59f3746dea9daca817a20f1212e075242d7e
Tres Walsh <tres.walsh@mnmlstc.com>
2013-12-13 14:12:03 -0800

    Add infix_ostream_iterator, allowing the printing of a list like [1, 2, 3]

--- include/core/iterator.hpp
+++ include/core/iterator.hpp
@@ -1,7 +1,9 @@
 #ifndef CORE_ITERATOR_HPP
 #define CORE_ITERATOR_HPP
 
+#include <functional>
 #include <iterator>
+#include <ostream>
 
 namespace core {
 inline namespace v1 {
@@ -45,6 +47,49 @@ template <class Container>
 auto crend (Container const& container) -> decltype(rend(container)) {
   return rend(container);
 }
+
+template <
+  class T,
+  class CharT=char,
+  class Traits=std::char_traits<CharT>
+> struct infix_ostream_iterator final : std::iterator<
+  std::output_iterator_tag,
+  void,
+  void,
+  void,
+  void
+> {
+  using ostream_type = std::basic_ostream<CharT, Traits>;
+  using traits_type = Traits;
+  using char_type = CharT;
+
+  infix_ostream_iterator (ostream_type& os) :
+    infix_ostream_iterator { os, nullptr }
+  { }
+
+  infix_ostream_iterator (ostream_type& os, char_type const* delimiter) :
+    os { os },
+    delimiter { delimiter },
+    first { true }
+  { }
+
+  infix_ostream_iterator& operator = (T const& item) {
+    if (not first and delimiter) { this->os.get() << delimiter; }
+    os.get() << item;
+    this->first = false;
+    return *this;
+  }
+
+  infix_ostream_iterator& operator ++ (int) { return *this; }
+  infix_ostream_iterator& operator ++ () { return *this; }
+  infix_ostream_iterator& operator * () { return *this; }
+
+private:
+  std::reference_wrapper<ostream_type> os;
+  char_type const* delimiter;
+  bool first;
+
+};
 
 }} /* namespace core::v1 */
 
--- tests/iterator.cpp
+++ tests/iterator.cpp
@@ -1,9 +1,23 @@
 #include <core/iterator.hpp>
+#include <algorithm>
+#include <ostream>
+#include <sstream>
 #include <vector>
 
 #include <cstdint>
 
 #include <unittest/unittest.hpp>
+
+template <class T>
+std::ostream& operator << (std::ostream& os, std::vector<T> const& vec) {
+  os << "[";
+  std::copy(
+    core::cbegin(vec),
+    core::cend(vec),
+    core::infix_ostream_iterator<T> { os, "," }
+  );
+  return os << "]";
+}
 
 int main () {
   using namespace unittest;
@@ -13,7 +27,12 @@ int main () {
     task("cbegin") = [value] {
       assert::equal(core::cbegin(value), value.begin());
     },
-    task("cend") = [value] { assert::equal(core::cend(value), value.end()); }
+    task("cend") = [value] { assert::equal(core::cend(value), value.end()); },
+    task("infix-ostream-iterator") = [value] {
+      std::ostringstream stream;
+      stream << value;
+      assert::equal(stream.str(), std::string { "[1,2,3,4,5]" });
+    }
   };
 
   monitor::run();
