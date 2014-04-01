38213f98b9a61afe3b3b07676db1b83199855c42
Tres Walsh <tres.walsh@mnmlstc.com>
2014-04-01 09:23:51 -0700

    Add test for <numeric> iota and <numeric> accumulate

--- tests/numeric.cpp
+++ tests/numeric.cpp
@@ -1,4 +1,6 @@
 #include <core/numeric.hpp>
+
+#include <functional>
 #include <vector>
 
 #include <unittest/unittest.hpp>
@@ -6,8 +8,24 @@
 int main () {
   using namespace unittest;
   test("numeric") = {
-    task("iota") = [] { assert::fail(); },
-    task("accumulate") = [] { assert::fail(); },
+    task("iota") = [] {
+      std::vector<int> values = { 0, 0, 0, 0, 0 };
+      core::iota(values, 0);
+      assert::equal(values[0], 0);
+      assert::equal(values[1], 1);
+      assert::equal(values[2], 2);
+      assert::equal(values[3], 3);
+      assert::equal(values[4], 4);
+    },
+
+    task("accumulate") = [] {
+      std::vector<int> values = { 1, 2, 3, 4 };
+      auto result = core::accumulate(values, 0);
+      assert::equal(result, 10);
+      result = core::accumulate(values, 1, std::multiplies<int> { });
+      assert::equal(result, 24);
+    },
+
     task("inner-product") = [] { assert::fail(); },
     task("adjacent-difference") = [] { assert::fail(); },
     task("partial-sum") = [] { assert::fail(); }
