ff5308efdd91a486afde2bd4dce45490b26d5cb9
Tres Walsh <tres.walsh@mnmlstc.com>
2014-04-02 10:57:27 -0700

    Add test for copy function in algorithm component

--- tests/algorithm.cpp
+++ tests/algorithm.cpp
@@ -1,4 +1,5 @@
 #include <core/algorithm.hpp>
+#include <vector>
 
 #include <unittest/unittest.hpp>
 
@@ -22,15 +23,34 @@ int main () {
     task("adjacent-find") = [] { assert::fail(); },
     task("search") = [] { assert::fail(); },
     task("search-n") = [] { assert::fail(); },
-    task("copy") = [] { assert::fail(); },
+
+    task("copy") = [] {
+      std::vector<int> from { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+      std::vector<int> to { };
+      std::ignore = core::copy(from, std::back_inserter(to));
+
+      assert::equal(to[0], 0);
+      assert::equal(to[1], 1);
+      assert::equal(to[2], 2);
+      assert::equal(to[3], 3);
+      assert::equal(to[4], 4);
+      assert::equal(to[5], 5);
+      assert::equal(to[6], 6);
+      assert::equal(to[7], 7);
+      assert::equal(to[8], 8);
+      assert::equal(to[9], 9);
+    },
+
     task("copy-if") = [] { assert::fail(); },
-    task("copy-n") = [] { assert::fail(); },
     task("copy-backward") = [] { assert::fail(); },
     task("move") = [] { assert::fail(); },
     task("move-backward") = [] { assert::fail(); },
     task("fill") = [] { assert::fail(); },
-    task("fill-n") = [] { assert::fail(); },
-    task("transform") = [] { assert::fail(); }
+    task("transform") = [] { assert::fail(); },
+    task("remove") = [] { assert::fail(); },
+    task("remove-if") = [] { assert::fail(); },
+    task("remove-copy") = [] { assert::fail(); },
+    task("remove-copy-if") = [] { assert::fail(); }
   };
 
   monitor::run();
