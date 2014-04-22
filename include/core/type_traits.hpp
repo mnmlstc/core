d3f803571fc29e387361cd51210b2cb5c23513d6
Tres Walsh <tres.walsh@mnmlstc.com>
2014-04-22 15:02:18 -0700

    Cleanup type traits component for issue #10

--- include/core/type_traits.hpp
+++ include/core/type_traits.hpp
@@ -9,11 +9,11 @@ inline namespace v1 {
 
 /* custom type traits */
 /* tuple_size is used by unpack, so we expect it to be available.
- * We also expect std::get<N> to be available for the give type T
+ * We also expect ::std::get<N> to be available for the give type T
  */
 template <class T>
 class is_unpackable {
-  template <class U> using tuple_size_t = typename std::tuple_size<U>::type;
+  template <class U> using tuple_size_t = typename ::std::tuple_size<U>::type;
   template <class U> static void check (tuple_size_t<U>*) noexcept;
   template <class> static void check (...) noexcept(false);
 public:
@@ -42,52 +42,54 @@ template <class T> struct result_of; /* SFINAE result_of */
 
 /* C++14 style aliases for standard traits */
 template <class T>
-using remove_volatile_t = typename std::remove_volatile<T>::type;
-
-template <class T> using remove_const_t = typename std::remove_const<T>::type;
-template <class T> using remove_cv_t = typename std::remove_cv<T>::type;
-
-template <class T> using add_volatile_t = typename std::add_volatile<T>::type;
-template <class T> using add_const_t = typename std::add_const<T>::type;
-template <class T> using add_cv_t = typename std::add_cv<T>::type;
+using remove_volatile_t = typename ::std::remove_volatile<T>::type;
 
 template <class T>
-using add_lvalue_reference_t = typename std::add_lvalue_reference<T>::type;
+using remove_const_t = typename ::std::remove_const<T>::type;
+template <class T> using remove_cv_t = typename ::std::remove_cv<T>::type;
 
 template <class T>
-using add_rvalue_reference_t = typename std::add_rvalue_reference<T>::type;
+using add_volatile_t = typename ::std::add_volatile<T>::type;
+template <class T> using add_const_t = typename ::std::add_const<T>::type;
+template <class T> using add_cv_t = typename ::std::add_cv<T>::type;
 
 template <class T>
-using remove_reference_t = typename std::remove_reference<T>::type;
+using add_lvalue_reference_t = typename ::std::add_lvalue_reference<T>::type;
 
 template <class T>
-using remove_pointer_t = typename std::remove_pointer<T>::type;
-
-template <class T> using add_pointer_t = typename std::add_pointer<T>::type;
+using add_rvalue_reference_t = typename ::std::add_rvalue_reference<T>::type;
 
 template <class T>
-using make_unsigned_t = typename std::make_unsigned<T>::type;
-template <class T> using make_signed_t = typename std::make_signed<T>::type;
+using remove_reference_t = typename ::std::remove_reference<T>::type;
 
 template <class T>
-using remove_extent_t = typename std::remove_extent<T>::type;
+using remove_pointer_t = typename ::std::remove_pointer<T>::type;
+
+template <class T> using add_pointer_t = typename ::std::add_pointer<T>::type;
 
 template <class T>
-using remove_all_extents_t = typename std::remove_all_extents<T>::type;
+using make_unsigned_t = typename ::std::make_unsigned<T>::type;
+template <class T> using make_signed_t = typename ::std::make_signed<T>::type;
 
-template <std::size_t Len, std::size_t Align>
-using aligned_storage_t = typename std::aligned_storage<Len, Align>::type;
+template <class T>
+using remove_extent_t = typename ::std::remove_extent<T>::type;
 
-template <class T> using decay_t = typename std::decay<T>::type;
+template <class T>
+using remove_all_extents_t = typename ::std::remove_all_extents<T>::type;
+
+template < ::std::size_t Len, ::std::size_t Align>
+using aligned_storage_t = typename ::std::aligned_storage<Len, Align>::type;
+
+template <class T> using decay_t = typename ::std::decay<T>::type;
 
 template <bool B, class T = void>
-using enable_if_t = typename std::enable_if<B, T>::type;
+using enable_if_t = typename ::std::enable_if<B, T>::type;
 
 template <bool B, class T, class F>
-using conditional_t = typename std::conditional<B, T, F>::type;
+using conditional_t = typename ::std::conditional<B, T, F>::type;
 
 template <class T>
-using underlying_type_t = typename std::underlying_type<T>::type;
+using underlying_type_t = typename ::std::underlying_type<T>::type;
 
 /* custom type trait specializations */
 template <class... Args> using invoke_of_t = typename invoke_of<Args...>::type;
@@ -103,58 +105,60 @@ template <class... Args> auto invoke_expr (undefined, Args&&...) -> undefined;
 
 template <class Functor, class Object, class... Args>
 auto invoke_expr (Functor&& fun, Object&& obj, Args&&... args) -> enable_if_t<
-  std::is_member_function_pointer<remove_reference_t<Functor>>::value and
-  std::is_base_of<
+  ::std::is_member_function_pointer<remove_reference_t<Functor>>::value and
+  ::std::is_base_of<
     class_of_t<remove_reference_t<Functor>>,
     remove_reference_t<Object>
   >::value,
-  decltype((std::forward<Object>(obj).*fun)(std::forward<Args>(args)...))
+  decltype((::std::forward<Object>(obj).*fun)(::std::forward<Args>(args)...))
 >;
 
 template <class Functor, class Object, class... Args>
 auto invoke_expr (Functor&& fun, Object&& obj, Args&&... args) -> enable_if_t<
-  std::is_member_function_pointer<remove_reference_t<Functor>>::value and
-  not std::is_base_of<
+  ::std::is_member_function_pointer<remove_reference_t<Functor>>::value and
+  not ::std::is_base_of<
     class_of_t<remove_reference_t<Functor>>,
     remove_reference_t<Object>
   >::value,
-  decltype(((*std::forward<Object>(obj)).*fun)(std::forward<Args>(args)...))
+  decltype(
+    ((*::std::forward<Object>(obj)).*fun)(::std::forward<Args>(args)...)
+  )
 >;
 
 template <class Functor, class Object>
 auto invoke_expr (Functor&& functor, Object&& object) -> enable_if_t<
-  std::is_member_object_pointer<remove_reference_t<Functor>>::value and
-  std::is_base_of<
+  ::std::is_member_object_pointer<remove_reference_t<Functor>>::value and
+  ::std::is_base_of<
     class_of_t<remove_reference_t<Functor>>,
     remove_reference_t<Object>
   >::value,
-  decltype(std::forward<Object>(object).*functor)
+  decltype(::std::forward<Object>(object).*functor)
 >;
 
 template <class Functor, class Object>
 auto invoke_expr (Functor&& functor, Object&& object) -> enable_if_t<
-  std::is_member_object_pointer<remove_reference_t<Functor>>::value and
-  not std::is_base_of<
+  ::std::is_member_object_pointer<remove_reference_t<Functor>>::value and
+  not ::std::is_base_of<
     class_of_t<remove_reference_t<Functor>>,
     remove_reference_t<Object>
   >::value,
-  decltype((*std::forward<Object>(object)).*functor)
+  decltype((*::std::forward<Object>(object)).*functor)
 >;
 
 template <class Functor, class... Args>
 auto invoke_expr (Functor&& functor, Args&&... args) -> decltype(
-  std::forward<Functor>(functor)(std::forward<Args>(args)...)
+  ::std::forward<Functor>(functor)(::std::forward<Args>(args)...)
 );
 
 template <bool, class... Args> struct invoke_of { };
 template <class... Args>
 struct invoke_of<true, Args...> {
-  using type = decltype(invoke_expr(std::declval<Args>()...));
+  using type = decltype(invoke_expr(::std::declval<Args>()...));
 };
 
 /* swappable implementation details */
-using std::declval;
-using std::swap;
+using ::std::declval;
+using ::std::swap;
 
 template <class T, class U>
 class is_swappable {
@@ -170,17 +174,17 @@ public:
 };
 
 template <class T, class U>
-struct is_nothrow_swappable : std::integral_constant<
+struct is_nothrow_swappable : ::std::integral_constant<
   bool,
   is_swappable<T, U>::value and noexcept(swap(declval<T&>(), declval<U&>()))
 > { };
 
 } /* namespace impl */
 
-template <class... Args> struct invokable : std::integral_constant<
+template <class... Args> struct invokable : ::std::integral_constant<
   bool,
-  not std::is_same<
-    decltype(impl::invoke_expr(std::declval<Args>()...)),
+  not ::std::is_same<
+    decltype(impl::invoke_expr(::std::declval<Args>()...)),
     impl::undefined
   >::value
 > { };
@@ -199,7 +203,9 @@ template <class... Ts> struct common_type;
 template <class T> struct common_type<T> { using type = decay_t<T>; };
 template <class T, class U>
 struct common_type<T, U> {
-  using type = decay_t<decltype(true ? std::declval<T>() : std::declval<U>())>;
+  using type = decay_t<
+    decltype(true ? ::std::declval<T>() : ::std::declval<U>())
+  >;
 };
 
 template <class T, class U, class... Ts>
@@ -213,18 +219,20 @@ struct common_type<T, U, Ts...> {
 template <class... T> using common_type_t = typename common_type<T...>::type;
 
 /* is_null_pointer */
-template <class T> struct is_null_pointer : std::false_type { };
+template <class T> struct is_null_pointer : ::std::false_type { };
 
 template <>
-struct is_null_pointer<std::nullptr_t const volatile> : std::true_type { };
+struct is_null_pointer<add_cv_t< ::std::nullptr_t>> : ::std::true_type { };
 template <>
-struct is_null_pointer<std::nullptr_t volatile> : std::true_type { };
-template <> struct is_null_pointer<std::nullptr_t const> : std::true_type { };
-template <> struct is_null_pointer<std::nullptr_t> : std::true_type { };
+struct is_null_pointer< ::std::nullptr_t volatile> : ::std::true_type { };
+template <>
+struct is_null_pointer< ::std::nullptr_t const> : ::std::true_type { };
+template <>
+struct is_null_pointer< ::std::nullptr_t> : ::std::true_type { };
 
 /* is_swappable */
 template <class T, class U=T>
-using is_swappable = std::integral_constant<
+using is_swappable = ::std::integral_constant<
   bool,
   impl::is_swappable<T, U>::value
 >;
@@ -236,21 +244,21 @@ using is_nothrow_swappable = impl::is_nothrow_swappable<T, U>;
 /* all-traits */
 template <class...> struct all_traits;
 template <class T, class... Args>
-struct all_traits<T, Args...> : std::integral_constant<bool,
+struct all_traits<T, Args...> : ::std::integral_constant<bool,
   T::value and all_traits<Args...>::value
 > { };
-template <> struct all_traits<> : std::true_type { };
+template <> struct all_traits<> : ::std::true_type { };
 
 /* any-traits */
 template <class...> struct any_traits;
 template <class T, class... Args>
-struct any_traits<T, Args...> : std::integral_constant<bool,
+struct any_traits<T, Args...> : ::std::integral_constant<bool,
   T::value or any_traits<Args...>::value
 > { };
-template <> struct any_traits<> : std::false_type { };
+template <> struct any_traits<> : ::std::false_type { };
 
 /* no-traits */
-template <class... Args> struct no_traits : std::integral_constant<bool,
+template <class... Args> struct no_traits : ::std::integral_constant<bool,
   not all_traits<Args...>::value
 > { };
 
